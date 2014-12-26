ccb76a06b9e6a50ff6963f76e08318c409e7e000
taukir4u <taukir4u@gmail.com>
2014-12-26 21:00:46 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ StudentInformationUIApp/StudentInformationUI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformationUIApp
+{
+    public partial class StudentInformationUI : Form
+    {
+        List<StudentInfo> students = new List<StudentInfo>();
+        public StudentInformationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StudentInfo studentObj = new StudentInfo();
+            studentObj.regNumber = regTextBox.Text;
+            studentObj.firstName = firstNameTextBox.Text;
+            studentObj.lastName = lastNameTextBox.Text;
+            students.Add(studentObj);
+            ClearTextBoxes();
+        }
+
+        private void ClearTextBoxes()
+        {
+            regTextBox.Text = "";
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+        }
+
+        private void buttonShowAll_Click(object sender, EventArgs e)
+        {
+            string columns = "Registration Number " + "Full Name";
+            string studentsInformation = "";
+
+            foreach (StudentInfo aStudent in students)
+            {
+                studentsInformation += aStudent.regNumber + "\t";
+                studentsInformation += aStudent.GetFullName();
+                studentsInformation += "\n";
+            }
+
+            MessageBox.Show(columns + "\n" + studentsInformation);
+        }
+    }
+}
